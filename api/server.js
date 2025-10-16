c703457e00669a88dbe9ed429187779255c3ed14
juanse1998 <juansesosa98@gmail.com>
2025-10-16 00:47:45 -0300

    fix cors

--- api/server.js
+++ api/server.js
@@ -13,8 +13,14 @@ const middlewares = jsonServer.defaults({
 
 server.use((req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept, Authorization');
   res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  
+  if (req.method === 'OPTIONS') {
+    return res.status(200).end();
+  }
+  
   next();
 });
 
